814b392e2577f57efa5a5682187b717a9847c0eb
Navneet Singh <mufc.navneet@gmail.com>
2023-07-27 15:28:01 +0530

    input output showcase done

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+var inputBox = document.querySelector("#input-box");
 var btnTest = document.querySelector("#btn-test");
+var outputBox = document.querySelector("#output");
+
+
 
 function clickEvent(){
-    console.log("Button has been clicked");
+
+    outputBox.innerText = "You typed: " + inputBox.value;
+
+    // console.log("Button has been clicked");
+    // console.log("input is", inputBox.value); // showing input of textarea in console
 }
 
 var btnOutput = btnTest.addEventListener("click", clickEvent);
+
+// outputBox.innerText = "So many things to learn";
 
